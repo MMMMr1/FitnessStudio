eeecfc54cc5d7e4bbd3f160b5bb22e4de3708e12
Michalenok Marina <mihalenokee@outlook.com>
2023-04-13 12:30:19 +0300

    updated ServiceConfiguration.java, application.yml (mail-service)

--- mail-service/src/main/java/it/academy/fitness_studio/configuration/ServiceConfiguration.java
+++ mail-service/src/main/java/it/academy/fitness_studio/configuration/ServiceConfiguration.java
@@ -2,6 +2,7 @@ package it.academy.fitness_studio.configuration;
 
 import it.academy.fitness_studio.service.*;
 import it.academy.fitness_studio.service.api.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +19,16 @@ import java.util.Properties;
 
 @Configuration
 public class ServiceConfiguration {
+    @Value("${mail.host}")
+    private String host;
+    @Value("${mail.port}")
+    private int port;
+    @Value("${mail.username}")
+    private String username;
+    @Value("${mail.password}")
+    private String password;
+    @Value("${mail.protocol}")
+    private String protocol;
 
     @Bean
     public IMailService emailService(
@@ -28,18 +39,12 @@ public class ServiceConfiguration {
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.mail.ru");
-        mailSender.setPort(465);
-
-        mailSender.setUsername("maksim.maks.23@mail.ru");
-        mailSender.setPassword("m8x7AeqawwuPHqybSrLQ");
-
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtps");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.ssl.enable", "true");
-        props.put("mail.debug", "true");
-
+        props.put("mail.transport.protocol", protocol);
         return mailSender;
     }
 }
