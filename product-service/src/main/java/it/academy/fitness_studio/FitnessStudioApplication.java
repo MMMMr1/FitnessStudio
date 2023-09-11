ac034571a6e5676a3a9825e81ad075109a16d280
Michalenok Marina <mihalenokee@outlook.com>
2023-09-11 14:08:27 +0300

    feat: register product-service as eureka client

--- product-service/src/main/java/it/academy/fitness_studio/FitnessStudioApplication.java
+++ product-service/src/main/java/it/academy/fitness_studio/FitnessStudioApplication.java
@@ -2,6 +2,7 @@ package it.academy.fitness_studio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FitnessStudioApplication {
 
 	public static void main(String[] args) {
