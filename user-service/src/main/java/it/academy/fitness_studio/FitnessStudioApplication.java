449c9d1f27ed05b93db5f63ae621c278e4e09458
Michalenok Marina <mihalenokee@outlook.com>
2023-09-11 14:07:55 +0300

    feat: register user-service as eureka client

--- user-service/src/main/java/it/academy/fitness_studio/FitnessStudioApplication.java
+++ user-service/src/main/java/it/academy/fitness_studio/FitnessStudioApplication.java
@@ -2,6 +2,8 @@ package it.academy.fitness_studio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -9,12 +11,12 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FitnessStudioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessStudioApplication.class, args);
 	}
-//
 	@Bean
 	public LocaleResolver localeResolver() {
 		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
